aa9c32b89e995f011839928a98d98dacaf5d4aee
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-07 01:17:22 +0530

    Authenticate SignupUser

--- task-springboot/src/main/java/com/application/task_springboot/services/auth/AuthService.java
+++ task-springboot/src/main/java/com/application/task_springboot/services/auth/AuthService.java
@@ -1,5 +1,12 @@
 package com.application.task_springboot.services.auth;
 
 
+import com.application.task_springboot.dto.SignUpRequest;
+import com.application.task_springboot.dto.UserDto;
+
 public interface AuthService {
+
+    UserDto signupUser(SignUpRequest signUpRequest);
+
+    boolean ha
 }
