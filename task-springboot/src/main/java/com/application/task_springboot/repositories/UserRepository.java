887336245c5df4c019637c2fff2b064d27acfce8
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-06 16:11:45 +0530

    Create UserRepository

---
+++ task-springboot/src/main/java/com/application/task_springboot/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.application.task_springboot.repositories;
+
+
+import com.application.task_springboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
