abe81d798d5a67837ce7bdee72f38f632a01c624
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-07-13 01:28:20 +0530

    initial commit

--- angular-task/src/app/app.module.ts
+++ angular-task/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './auth/components/login/login.component';
 import { SignupComponent } from './auth/components/signup/signup.component';
 import { DemoAngularMaterialModule } from './DemoAngularMaterialModule';
