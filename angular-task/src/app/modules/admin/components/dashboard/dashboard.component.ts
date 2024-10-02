62af509026317171aef66380de252f0625381414
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-10-02 18:09:11 +0530

    calling search task in Angular

--- angular-task/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ angular-task/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,9 +11,16 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class DashboardComponent {
 
   listOfTasks: any = [];
+  searchForm!: FormGroup;
 
-  constructor(private service: AdminService, private snackbar: MatSnackBar) {
+  constructor(private service: AdminService, 
+    private snackbar: MatSnackBar,
+  private fb: FormBuilder) 
+  {
     this.getTasks();
+    this.searchForm = this.fb.group({
+      title: [null]
+    })
    }
 
    getTasks() {
@@ -27,4 +35,14 @@ export class DashboardComponent {
       this.getTasks();
     });
    }
+
+   searchTask(){
+    this.listOfTasks = [];
+    const title = this.searchForm.get('title')!.value;
+    this.service.searchTask(title).subscribe((res)=>{
+      console.log(res);
+      this.listOfTasks = res;
+    })
+   }
+
 }
